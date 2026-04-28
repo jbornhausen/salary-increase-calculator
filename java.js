ae4dfa9cb040c8231d4d1d09a32861c75613ed8a
jbornhausen <juliana.girardello@Hotmail.com>
2026-04-28 16:47:40 -0300

    Arrumando o JS

--- java.js
+++ java.js
@@ -1,19 +1,28 @@
-/* var nome = window.prompt("Qual é o seu nome?")
-var salário = Number(window.prompt("Qual é o seu salário?"))
-var tempoDeEmpresa = window.prompt ("Quanto tempo você tem de empresa?")
+function calcular () {
+var nome = document.getElementById('nome').value
+var salario = Number (document.getElementById('salario').value)
+var tempo = Number(document.getElementById ('tempo').value)
 
-var aumento = tempoDeEmpresa <= 3 ? salário * 0.05 
-: tempoDeEmpresa <= 10 ?
-salário * 0.1 : salário * 0.15
+var aumento = tempo <= 3 ? salario * 0.05 
+: tempo <= 10 ?
+salario * 0.1 : salario * 0.15
 
-var porcentagem = tempoDeEmpresa <= 3? '5%' : 
-tempoDeEmpresa <= 10 ?
+var porcentagem = tempo <= 3? '5%' : 
+tempo <= 10 ?
 '10%' : '15%'
 
-document.write (
-    `Olá ${nome.toUpperCase()} <p>
-    Salário atual ${salário.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <p> 
-    A porcentagem de aumento foi de ${porcentagem} <p>
-    Seu aumento será de  ${aumento.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <p>
-    Totalizando ${(salário + aumento).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} <p>
-    `) *\
+var resultado = document.getElementById('resultado')
+
+resultado.innerHTML =
+    `Olá ${nome.toUpperCase()} 
+    <br>
+    Salário atual ${salario.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} 
+    <br>
+    A porcentagem de aumento foi de ${porcentagem} 
+    <br>
+    Seu aumento será de  ${aumento.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} 
+    <br>
+    Totalizando ${(salario + aumento).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})} 
+    `
+     resultado.style.display = 'block'
+  }
